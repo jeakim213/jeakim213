b1894afbfe75357d1fd1539b93c221d08ccfb64e
Jihye Kim <kjhye213@naver.com>
2022-04-15 20:32:56 +0900

    cote

---
+++ java/codding/src/coddingTest/lottos.java
@@ -0,0 +1,29 @@
+package coddingTest;
+
+public class lottos{
+    public static void main(String[] args) {
+        int[] lottos = {1, 0, 43, 0, 0, 23};
+        int[] winNum = {3, 5, 23, 17, 37, 43};
+        int[] result = solution(lottos, winNum);
+        System.out.printf("최고순위 : %d, 최저순위 : %d", result[0], result[1]);
+
+    }
+    public static int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {1,2};
+        int count = 7, zero = 0;
+        for(int i = 0; i < lottos.length; i++){
+            for (int j = 0; j < win_nums.length; j++){
+                if (lottos[i] == win_nums[j])
+                    count--;
+            }
+            if(lottos[i] == 0)
+                zero++;
+        }
+        if (count == 7)
+            answer[1] = 6;
+        else
+            answer[1] = count;
+        answer[0] =  count - zero;
+        return answer;
+    }
+}
